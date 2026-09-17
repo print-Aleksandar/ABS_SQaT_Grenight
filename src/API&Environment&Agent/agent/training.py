4e784424d316e6950e955781a53a62632145beae
print-Aleksandar <aleksandartodoroski@zohomail.eu>
2026-09-17 22:42:23 +0200

    self play absolute perspective vs canonical training 22

--- src/API&Environment&Agent/agent/training.py
+++ src/API&Environment&Agent/agent/training.py
@@ -335,4 +335,4 @@ def train_agent(is_self_play: bool,
         save_checkpoint(agent, episode, agent_step, is_double_net)
         print("Done.")
 
-train_agent(True, True, False, True, True, False)
+train_agent(True, True, False, True, False, False)
