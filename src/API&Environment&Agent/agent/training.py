e1f94fed3be37c08b06552f11e73f3a5cbf9749a
print-Aleksandar <aleksandartodoroski@zohomail.eu>
2026-09-18 11:06:18 +0200

    canonical reward shaping residual dueling ddqn training

--- src/API&Environment&Agent/agent/training.py
+++ src/API&Environment&Agent/agent/training.py
@@ -347,4 +347,4 @@ def train_agent(is_self_play: bool,
         save_checkpoint(agent, episode, agent_step, is_double_net)
         print("Done.")
 
-train_agent(True, True, False, True, True, True)
+train_agent(True, True, True, True, True, True)
